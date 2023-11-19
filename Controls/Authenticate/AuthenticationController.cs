ded6a8d7cc948a09db84cd31c6e0dcacbfdfea54
liqngliz <liqngliz@gmail.com>
2023-11-19 14:22:03 +0100

    first commit

---
+++ Controls/Authenticate/AuthenticationController.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace IDSP_Boiler_Plate;
+
+public class AuthenticationController : IController<JObject, JObject>
+{   
+    public JObject Process(JObject Input)
+    {
+        return Input;
+    }
+}
