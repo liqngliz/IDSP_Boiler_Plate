7b10809e1bbce8502a96631510f69e4aa6c12f14
liqngliz <liqngliz@gmail.com>
2023-11-22 15:15:48 +0100

    access tokens

--- Controls/Authenticate/AuthenticationController.cs
+++ Controls/Authenticate/AuthenticationController.cs
@@ -11,8 +11,14 @@ public class AuthenticationController : IController<string, HttpContext>
         var headers = Input.Request.Headers;
         StringValues principalName;
         StringValues principalClaims;
+        StringValues idToken;
+        StringValues accessToken;
+        StringValues expiresOn;
         headers.TryGetValue("X-MS-CLIENT-PRINCIPAL-NAME", out principalName);
         headers.TryGetValue("X-MS-CLIENT-PRINCIPAL", out principalClaims);
+        headers.TryGetValue("X-MS-TOKEN-AAD-ID-TOKEN", out idToken);
+        headers.TryGetValue("X-MS-TOKEN-AAD-ACCESS-TOKEN", out accessToken);
+        headers.TryGetValue("X-MS-TOKEN-AAD-EXPIRES-ON", out expiresOn);
         byte[] data = Convert.FromBase64String(principalClaims.ToString());
         string decodedString = System.Text.Encoding.UTF8.GetString(data);
 
@@ -20,6 +26,9 @@ public class AuthenticationController : IController<string, HttpContext>
         stringBuilder.AppendLine("Hello " + principalName.ToString());
         stringBuilder.AppendLine("Your OAuth Claims Tokens: ");
         stringBuilder.AppendLine(decodedString);
+        stringBuilder.AppendLine("Id Token: " + idToken.ToString());
+        stringBuilder.AppendLine("Access Token: " + accessToken.ToString());
+        stringBuilder.AppendLine("Expires: " + expiresOn.ToString());
         return  stringBuilder.ToString();
     }
 }
