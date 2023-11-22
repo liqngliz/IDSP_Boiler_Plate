8ed7c7551bd9dfc72df3c475142bed7a2cbd52b4
liqngliz <liqngliz@gmail.com>
2023-11-22 07:35:30 +0100

    Authentication controller exposes user claims

--- Controls/Authenticate/AuthenticationController.cs
+++ Controls/Authenticate/AuthenticationController.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System.Text;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json.Linq;
 
 namespace IDSP_Boiler_Plate;
 
@@ -7,7 +9,17 @@ public class AuthenticationController : IController<string, HttpContext>
     public string Process(HttpContext Input)
     {   
         var headers = Input.Request.Headers;
+        StringValues principalName;
+        StringValues principalClaims;
+        headers.TryGetValue("X-MS-CLIENT-PRINCIPAL-ID", out principalName);
+        headers.TryGetValue("X-MS-CLIENT-PRINCIPAL", out principalClaims);
+        byte[] data = Convert.FromBase64String(principalClaims.ToString());
+        string decodedString = System.Text.Encoding.UTF8.GetString(data);
 
-        return JsonConvert.SerializeObject(headers);
+        var stringBuilder = new StringBuilder();
+        stringBuilder.AppendLine("Hello " + principalName.ToString());
+        stringBuilder.AppendLine("Your OAuth Claims Tokens: ");
+        stringBuilder.AppendLine(decodedString);
+        return  stringBuilder.ToString();
     }
 }
